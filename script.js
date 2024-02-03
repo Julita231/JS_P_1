064d12b2ca120b01b5692b00f0ee1e9038fed3b4
Julita <julita231@hotmail.com>
2024-02-03 09:11:39 +0100

    index

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const incomeValue = document.getElementById("income-value");
 const incomeList = document.getElementById("income-list");
 const incomeArray = [];
 
+const expenseForm = document.getElementById("expenseForm");
+const expenseName = document.getElementById("expense-name");
+const expenseValue = document.getElementById("expense-value");
+const expenseList = document.getElementById("expense-list");
+const expenseArray = [];
+
 incomeForm.addEventListener("submit", (event) => {
   event.preventDefault();
   addIncome();
@@ -29,10 +35,16 @@ const displayIncomes = () => {
     const valueParagraph = document.createElement("p");
     const editButton = document.createElement("button");
     const deleteButton = document.createElement("button");
+
     nameParagraph.textContent = income.name;
     valueParagraph.textContent = income.amount;
+
     editButton.textContent = "Edytuj";
     deleteButton.textContent = "Usuń";
+
+    li.className = "form";
+    nameParagraph.className = "income";
+    valueParagraph.className = "income";
 
     li.appendChild(nameParagraph);
     li.appendChild(valueParagraph);
@@ -41,6 +53,7 @@ const displayIncomes = () => {
     incomeList.appendChild(li);
 
     deleteButton.addEventListener("click", () => removeIncome(income.id));
+    editButton.addEventListener("click", () => editIncome(income, li));
   });
 };
 
@@ -48,6 +61,145 @@ const removeIncome = (id) => {
   const indexToRemove = incomeArray.findIndex((item) => item.id === id);
   console.log(indexToRemove);
   incomeArray.splice(indexToRemove, 1);
-  console.log(incomeArray);
+  console.log(removeIncome);
   displayIncomes();
+};
+
+const editIncome = (income, li) => {
+  const editForm = document.createElement("form");
+  const nameInput = document.createElement("input");
+  const valueInput = document.createElement("input");
+  const saveButton = document.createElement("button");
+  const cancelButton = document.createElement("button");
+
+  nameInput.value = income.name;
+  valueInput.value = income.amount;
+  saveButton.textContent = "Zapisz";
+  saveButton.type = "submit";
+  cancelButton.textContent = "Anuluj";
+  cancelButton.type = "button";
+  editForm.className = "edit-form";
+
+  editForm.appendChild(nameInput);
+  editForm.appendChild(valueInput);
+  editForm.appendChild(saveButton);
+  editForm.appendChild(cancelButton);
+
+  li.appendChild(editForm);
+
+  cancelButton.addEventListener("click", () => {
+    li.removeChild(editForm);
+  });
+
+  editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    incomeArray.find((item) => {
+      if (item.id === income.id) {
+        item.name = nameInput.value;
+        item.amount = valueInput.value;
+        displayIncomes();
+      }
+    });
+  });
+};
+
+// funkcje dodawania:
+
+const incomesSum = incomesArr.reduce(
+  (acc, currentValue) => acc + currentValue.amount,
+  0
+);
+console.log(incomesSum);
+
+//poniżej js do wydatków
+
+expenseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addExpense();
+});
+
+const addExpense = () => {
+  const expense = {
+    name: expenseName.value,
+    amount: Number(expenseValue.value),
+    id: uuidv4(),
+  };
+  expenseArray.push(expense);
+  displayExpenses();
+};
+
+const displayExpenses = () => {
+  expenseList.innerHTML = "";
+  expenseArray.forEach((expense) => {
+    const li = document.createElement("li");
+    const nameParagraph = document.createElement("p");
+    const valueParagraph = document.createElement("p");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    nameParagraph.textContent = expense.name;
+    valueParagraph.textContent = expense.amount;
+
+    editButton.textContent = "Edytuj";
+    deleteButton.textContent = "Usuń";
+
+    li.className = "form";
+    nameParagraph.className = "income";
+    valueParagraph.className = "income";
+
+    li.appendChild(nameParagraph);
+    li.appendChild(valueParagraph);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    expenseList.appendChild(li);
+
+    deleteButton.addEventListener("click", () => removeExpense(expense.id));
+    editButton.addEventListener("click", () => editExpense(expense, li));
+  });
+};
+
+const removeExpense = (id) => {
+  const indexToRemove = expenseArray.findIndex((item) => item.id === id);
+
+  expenseArray.splice(indexToRemove, 1);
+
+  displayExpenses();
+};
+
+const editExpense = (expense, li) => {
+  const editForm = document.createElement("form");
+  const nameInput = document.createElement("input");
+  const valueInput = document.createElement("input");
+  const saveButton = document.createElement("button");
+  const cancelButton = document.createElement("button");
+
+  nameInput.value = expense.name;
+  valueInput.value = expense.amount;
+  saveButton.textContent = "Zapisz";
+  saveButton.type = "submit";
+  cancelButton.textContent = "Anuluj";
+  cancelButton.type = "button";
+  editForm.className = "edit-form";
+
+  editForm.appendChild(nameInput);
+  editForm.appendChild(valueInput);
+  editForm.appendChild(saveButton);
+  editForm.appendChild(cancelButton);
+
+  li.appendChild(editForm);
+
+  cancelButton.addEventListener("click", () => {
+    li.removeChild(editForm);
+  });
+
+  editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    expenseArray.find((item) => {
+      if (item.id === expense.id) {
+        item.name = nameInput.value;
+        item.amount = valueInput.value;
+        displayExpenses();
+      }
+    });
+  });
 };
